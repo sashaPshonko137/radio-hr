072ef4078eb857ed503361732fbf38dd375fabae
sashaPshonko137 <privetnitron@gmail.com>
2025-08-24 03:12:57 +0500

    dfgfdgfddf

---
+++ oi.sh
@@ -0,0 +1,8 @@
+# Скачать в домашнюю папку
+wget https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -O ~/yt-dlp
+
+# Дать права на выполнение
+chmod +x ~/yt-dlp
+
+# Запускать с указанием полного пути
+~/yt-dlp --version
