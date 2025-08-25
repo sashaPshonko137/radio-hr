bcae294f154cea9345cecb66341e26c2f7c0dfb3
sashaPshonko137 <privetnitron@gmail.com>
2025-08-25 19:35:22 +0500

    jksdankjnsdajnkasd

---
+++ hz.sh
@@ -0,0 +1,5 @@
+# Скачайте звуковой шрифт в папку проекта
+cd ~/radio-hr/timidity
+wget https://ftp.debian.org/debian/pool/contrib/t/timidity-freepats/timidity-freepats_20060219-3_all.deb
+ar x timidity-freepats_20060219-3_all.deb
+tar -xf data.tar.xz
